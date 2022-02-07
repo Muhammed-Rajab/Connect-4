d5090a75d8953d79ec788d50ba88a33c790803eb
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-02-08 01:28:27 +0530

    Created a basic UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+const log = console.log;
+
+function startGame() {
+
+    /* Query selectors */
+    const grid = document.querySelector("#grid");
+    const holes = document.querySelectorAll(".holes");
+    const scoreSpan = document.querySelector(".score");
+    const activePlayerDisplay = document.querySelector(".player-turn");
+
+    /* Variables */
+    const ROW = 6;
+    const COL = 7;
+
+    let currentPlayer = "player-1";
+
+
+    function generateHoles(col, row){
+    
+        for(let i = 0; i < row*col; i++){
+            const newHole = document.createElement("div");
+            newHole.classList.add("holes");
+            grid.appendChild(newHole);
+        };
+    };
+
+    generateHoles(COL, ROW);
+};
+
+window.addEventListener("load", startGame);
